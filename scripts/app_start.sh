81f691596d8ad8394a7f8d479c4474c0b1a1cfd6
Martin Todorov <martin.todorov@kinsend.io>
2023-05-04 03:13:04 +0300

    Set up log rotation configuration for `pm2`. When the logs hit 128M, they will zipped and truncated.

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
 cd /home/ec2-user/kinsend-api
+
+# Install pm2-logrotate
+pm2 install pm2-logrotate
+# Set max log size to 128M
+pm2 set pm2-logrotate:max_size 128M
+# Enable compression of logs when rotated
+pm2 set pm2-logrotate:compress true
+
+# Start the app
 pm2 start kinsend-api-pm2.json
