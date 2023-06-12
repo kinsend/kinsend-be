d1b09a19b21d7d7e6188db60a97407ea48cfc11b
anthonyfrancis-zenkoders <anthony.francis@zenkoders.com>
2023-06-12 18:49:40 +0500

    removed unwanted code

--- src/modules/cname/services/CNAMEDeleteByIdAction.service.ts
+++ src/modules/cname/services/CNAMEDeleteByIdAction.service.ts
@@ -4,17 +4,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { AmplifyClientService } from '../../../shared/services/amplify.client.service';
 import { NotFoundException } from '../../../utils/exceptions/NotFoundException';
 import { RequestContext } from '../../../utils/RequestContext';
 import { CNAME, CNAMEDocument } from '../cname.schema';
 
 @Injectable()
 export class CNAMEDeleteByIdAction {
-  constructor(
-    @InjectModel(CNAME.name) private cnameModel: Model<CNAMEDocument>,
-    private amplifyClientService: AmplifyClientService,
-  ) {}
+  constructor(@InjectModel(CNAME.name) private cnameModel: Model<CNAMEDocument>) {}
 
   async execute(context: RequestContext, id: string): Promise<void> {
     const { user } = context;
@@ -25,7 +21,6 @@ export class CNAMEDeleteByIdAction {
     if (!cnameExist) {
       throw new NotFoundException('CNAME', 'CNAME not found!');
     }
-    await this.amplifyClientService.deleteSubDomain(context, cnameExist.title);
     await cnameExist.delete();
   }
 }
