3c05c30dc2fe5b65738361457e5e524a4c86f009
kyratking <kyratking944@gmail.com>
2023-10-18 14:40:48 +0500

    #138: Validation for CNAME

--- src/shared/services/validate.domain.service.ts
+++ src/shared/services/validate.domain.service.ts
@@ -1,31 +1,21 @@
-/* eslint-disable unicorn/prevent-abbreviations */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
-import {
-  ValidatorConstraint,
-  ValidatorConstraintInterface,
-  ValidationArguments,
-} from 'class-validator';
-import { ConfigService } from '../../configs/config.service';
+import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
 
 @ValidatorConstraint({ async: true })
 @Injectable()
 export class ValidateDomainService implements ValidatorConstraintInterface {
-  constructor(private readonly configService: ConfigService) {}
-
-  validate(text: string, args: ValidationArguments) {
+  validate(text: string) {
     const regex =
-      // eslint-disable-next-line unicorn/better-regex
-      /^[a-zA-Z0-9][a-zA-Z0-9-_]{0,61}[a-zA-Z0-9]{0,1}\.([a-zA-Z]{1,6}|[a-zA-Z0-9-]{1,30})+\.([a-zA-Z]{1,6}|[a-zA-Z0-9-]{1,30}\.[a-zA-Z]{2,3})$/g;
+      /^(?!-)[\w-](?!-)(?:[\w-]{0,61}[\da-z])?(?:\.(?!-)[\w-](?!-)(?:[\w-]{0,61}[\da-z])?)*$/gi;
 
-    const isValid = `${text}.${this.configService.domain}`.match(regex);
+    const isValid = text.match(regex);
     if (isValid && isValid.length > 0) {
       return true;
     }
     return false;
   }
 
-  defaultMessage(args: ValidationArguments) {
+  defaultMessage() {
     // here you can provide default error message if validation failed
     return 'Domain ($value) is not correct format!';
   }
